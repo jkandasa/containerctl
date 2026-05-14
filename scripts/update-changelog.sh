9df86438d7fc218cd77942e126a500d3c30309c8
Jeeva Kandasamy <jkandasa@gmail.com>
2026-05-14 23:59:43 +0530

    fix release script
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -4,17 +4,32 @@ set -euo pipefail
 VERSION="$1"
 DATE=$(date +%Y-%m-%d)
 
+# Moves content from [Unreleased] into the new version section and clears [Unreleased].
 awk -v ver="$VERSION" -v date="$DATE" '
-/^## \[Unreleased\]/ { seen=1 }
-/^---$/ && seen && !done {
+/^## \[Unreleased\]$/ {
+    in_unreleased = 1
     print
-    print ""
-    print "## [" ver "] - " date
-    print ""
-    print "---"
-    done=1
     next
 }
+
+in_unreleased && /^---$/ {
+    in_unreleased = 0
+    # Empty [Unreleased] section followed by new versioned section
+    print ""
+    print "---"
+    print ""
+    print "## [" ver "] - " date
+    printf "%s", content
+    print "---"
+    content = ""
+    next
+}
+
+in_unreleased {
+    content = content $0 "\n"
+    next
+}
+
 { print }
 ' CHANGELOG.md > CHANGELOG.tmp && mv CHANGELOG.tmp CHANGELOG.md
 
